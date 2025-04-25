0d020fec30c6d8a1fdad16988fc9a56efef75970
carlito208sueca <carlescarbonellsales@gmail.com>
2025-04-25 11:03:35 +0200

    TrainGPU

--- datasets/YOLOTrainer.py
+++ datasets/YOLOTrainer.py
@@ -1,3 +1,3 @@
 from ultralytics import YOLO
-model = YOLO('yolo11n.yaml')
-model.train(data='coco8.yaml', epochs=3)
+model = YOLO('yolo11n.pt')
+model.train(data='coco8.yaml', epochs=60, device=0)
--- datasets/ultralitics.py
+++
@@ -1,3 +0,0 @@
-from ultralytics import YOLO
-model = YOLO('yolo11n.pt')
-model.train(data='coco8.yaml', epochs=60, device=0)
